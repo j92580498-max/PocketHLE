29591448974ad1e9715e7b589ce877035611d0d3
zocomputer <help@zocomputer.com>
2026-08-05 22:14:06 +0000

    fix: initialize pointer transition in tests

--- crates/pocket-winceapi/src/coredll.rs
+++ crates/pocket-winceapi/src/coredll.rs
@@ -12762,6 +12762,7 @@ mod tests {
             pointer_x: 0,
             pointer_y: 0,
             pointer_down: false,
+            pointer_button_transition: false,
             pointer_capture: 0,
             gapi_keys_queried: false,
             pending_message: None,
